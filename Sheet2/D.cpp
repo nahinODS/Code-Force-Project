ab50e9b786c24b9535107b91f99b35616ac5b88e
MD Abdulah Al Nahin <abdullahalnahin23@gmail.com>
2025-10-29 10:23:34 +0600

    Create D.cpp

---
+++ Sheet2/D.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        long long a;
+        cin>>a;
+        if( a == 1999)
+        {
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else{
+        cout<<"Wrong"<<endl;
+    }
+}
+}
