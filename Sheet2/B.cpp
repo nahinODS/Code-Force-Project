2f921a6268736c2eccd9a0720fd9212505d8f1d9
MD Abdulah Al Nahin <abdullahalnahin23@gmail.com>
2025-10-29 10:20:48 +0600

    Update B.cpp

--- Sheet2/B.cpp
+++ Sheet2/B.cpp
@@ -1,1 +1,12 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long n;
+  cin >> n;
+  if(n<2)cout << -1 <<endl;
+  for( long long i = 1; i <=n; i++){
+        if(i % 2 ==0)
+    cout<< i << endl;
+  }
+  return 0;
+  
